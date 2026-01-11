4b1ca807268852e201d7c8bfc2ba2f9e92aeb5c6
hunglp9397 <lephihung0997@gmail.com>
2026-01-11 21:55:41 +0700

    update pom

--- consumer/src/main/java/com/hunglp/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/hunglp/consumer/ConsumerApplication.java
@@ -2,10 +2,8 @@ package com.hunglp.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableRetry
 public class ConsumerApplication {
 
     public static void main(String[] args) {
